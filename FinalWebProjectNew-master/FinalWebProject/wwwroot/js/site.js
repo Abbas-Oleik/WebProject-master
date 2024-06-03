94dde571a7054cd2dfb047877dfa9e9ad9a316a7
Abbas Olleik <olleikolleik968@gmail.com>
2024-06-03 12:22:17 -0700

    Initial commit

---
+++ FinalWebProjectNew-master/FinalWebProject/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+/*
+document.getElementById("#close-sidebar").addEventListener("click", closeOnClick)
+function closeOnClick(e) {
+
+    console.log("heyyyyeyyeyeyeyeyyeyeyeyey")
+}*/
+console.log('hello world');
+const close_popin = document.querySelector("#close-sidebar");
+close_popin.addEventListener("click", function () {
+    console.log("hey");
+})
